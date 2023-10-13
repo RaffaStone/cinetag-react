006879094c9b185bb3bea7c5ed3b4ab633dfaf19
RaffaStone <rafael.dias004@hotmail.com>
2023-10-13 12:07:35 -0300

    upando o projeto

---
+++ src/pages/Favoritos/index.js
@@ -0,0 +1,25 @@
+import Banner from 'components/Banner';
+import styles from './Favoritos.module.css'
+import Titulo from 'components/Titulo';
+import { useFavoritoContext } from 'components/contextos/Favoritos';
+import Card from 'components/Card';
+
+
+function Favoritos() {
+    const { favorito } = useFavoritoContext();
+    return(
+        <>
+            <Banner imagem="favoritos"/> 
+            <Titulo>
+                <h1>Meus Favoritos</h1>
+            </Titulo>
+            <section className={styles.container}>
+                {favorito.map((fav) => {
+                    return <Card {...fav} key={fav.id} />
+                })}     
+            </section>
+        </>
+    )
+}
+
+export default Favoritos;   
